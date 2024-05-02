6da673a5ba0c6f7feb07fb33e5aba42a705c7183
Jithin Vyas <68466206+jithinvyas@users.noreply.github.com>
2024-05-02 15:37:13 +0530

    Update1

--- Advance_SQL/string_split VS string_agg.sql
+++ Advance_SQL/string_split VS string_agg.sql
@@ -1,3 +1,16 @@
+-- Data
+CREATE TABLE Employee (
+    ID INT,
+    Name NVARCHAR(100),
+    Gender NVARCHAR(10),
+    Salary INT
+);
+INSERT INTO Employee (ID, Name, Gender, Salary) VALUES
+		(1, 'Sara Nani', 'Female', 4500),
+		(2, 'James Histo', 'Male', 5300),
+		(3, 'Mary Jane', 'Female', 6200),
+		(4, 'Paul Sensit', 'Male', 4200),
+		(5, 'Mike Jen', 'Male', 5500);
 select * from tblEmployee
 
 -- ID	Name		Gender	Salary
@@ -8,17 +21,19 @@ select * from tblEmployee
 -- 5	Mike Jen	Male	5500
 
 
+-- Collect the Names of all Male & Female Employees
 select 
 	Gender, 
-	string_agg(Name,', ') as Namelst 
+	string_agg(Name,', ') as Namelst            ---> Grouping
 from tblEmployee
 group by Gender;
 
 -- Gender	 Namelst
 -- Female	 Sara Nani, Mary Jane
--- Male	Paul Sensit, Mike Jen, James Histo
+-- Male		 Paul Sensit, Mike Jen, James Histo
 
 
+--Decoding/Reverse the above process
 with cte 
 as(
 	select 
@@ -31,12 +46,12 @@ select
 	Gender, 
 	value as Names 
 from cte
-cross apply string_split(Namelst,',') 
+cross apply string_split(Namelst,',')            ---> Way of Joining Functions
 order by Names
 
--- gender	Names
+-- Gender	Names
 -- Male	    James Histo
--- Female	Mary Jane
+-- Female   Mary Jane
 -- Male	    Mike Jen
 -- Male	    Paul Sensit
--- Female	Sara Nani
+-- Female   Sara Nani
